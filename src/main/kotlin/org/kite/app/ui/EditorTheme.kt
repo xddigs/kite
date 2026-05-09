9c3ceef7dd697bbcae4b326c7c18c8f5b4cae906
diego <sargent.pointed453@passinbox.com>
2026-05-09 16:04:57 +0200

    Add missing ref

---
+++ src/main/kotlin/org/kite/app/ui/EditorTheme.kt
@@ -0,0 +1,16 @@
+package org.kite.app.ui
+
+import java.awt.Color
+
+object EditorTheme {
+    val BACKGROUND_COLOR = Color(36, 39, 58, 255)
+    val TEXT_COLOR = Color(220, 220, 220)
+    val CURSOR_COLOR = Color(255, 255, 255)
+    val SELECTION_COLOR = Color(100, 100, 255, 100)
+
+    val LINE_NUMBER_COLOR = Color(255, 255, 255)
+
+    val KEYWORD_COLOR = Color(142, 95, 241)
+    val STRING_COLOR = Color(0, 219, 174)
+    val COMMENT_COLOR = Color(87, 166, 74)
+}
